bf9da859ad43dc4e958c7950d866f0945982fc87
dev <hizlle1817@gmail.com>
2021-02-13 19:11:43 +0400

    using dependency injection

--- src/main/java/DependencyInjection/Employee.java
+++ src/main/java/DependencyInjection/Employee.java
@@ -4,7 +4,17 @@ import Pet.Animal;
 
 public class Employee {
     private Animal pet;
+    private String name;
+    private int age;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+
+        this.age = age;
+    }
 
     public Employee(Animal pet) {
         this.pet = pet;
@@ -14,5 +24,13 @@ public class Employee {
     public void couldYourPet() {
         System.out.println("Call a pet ");
         pet.say();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
     }
 }
